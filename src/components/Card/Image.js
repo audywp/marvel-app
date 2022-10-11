e06570ee820cc2622d63d2cdad72aeb215406f48
audy <audywisudapratama@gmail.com>
2022-10-12 01:37:20 +0700

    add card component

---
+++ src/components/Card/Image.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+export default function CardImage({ url, title, refID }) {
+  const Image = styled.div`
+    background-image: url("${url}");
+    height: 300px;
+    width: 100%;
+    cursor: pointer;
+  `;
+
+  const Container = styled.div`
+    width: calc(100% / 5);
+  `;
+
+  const navigate = useNavigate();
+
+  const getByID = () => {
+    navigate(`/id?reference=${refID}`);
+  };
+
+  return (
+    <Container
+      onClick={getByID}
+      className="flex flex-col justify-center items-center px-10"
+    >
+      <Image className="bg-center bg-cover bg-no-repeat shadow-lg rounded-xl mb-4" />
+      <p className="font-bold mb-16 truncate w-60">{title}</p>
+    </Container>
+  );
+}
